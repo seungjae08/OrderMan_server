67b5521efce3f2d2a1156456d8a4777f277fa828
KimJaekyoon <kjk704@gmail.com>
2020-12-09 15:39:40 +0900

    optimized some codes

--- controllers/order/market.js
+++ controllers/order/market.js
@@ -9,79 +9,74 @@ const {
 
 module.exports = {
   post: async (req, res) => {
-    // 회원 비회원 분기 처리
-    try{
+    try {
+      // 쿠키에 담긴 jwt의 body를 decode해서 사용자 ID 확인
       const JWT = jwt.verify(req.cookies.accessToken, secret.secret_jwt);
+      // 1. 회원분기 처리: 일반회원은 standard
       if (req.cookies.userType === "standard") {
-        // 쿠키에 담긴 jwt의 body를 decode해서 사용자 ID 확인
-        // 받은 데이터 확인
+        // POST 데이터 확인
         const { mobile } = req.body;
-
         // 사용자 ID로 user테이블의 id 찾기
         const userSelected = await user.findOne({
-	  attributes:["id"],
+          attributes: ["id"],
           where: { userId: JWT.userId },
           raw: true
         });
+        // 선호마트정보 기입
         let [mart, b] = await market.findOrCreate({
           where: { mobile: mobile },
           defaults: { mobile: mobile },
           raw: true
         })
-
         // 위의 id를 기반으로 user_order 테이블에 데이터 기록
-  
-        
         let [result, created] = await user_market.findOrCreate({
           where: { userId: userSelected.id },
-          defaults : { userId: userSelected.id, marketId: mart.id},
+          defaults: { userId: userSelected.id, marketId: mart.id },
           raw: true
-        }).catch(err=>{res.status(404).send(err)})
+        }).catch(err => { res.status(404).send(err) })
 
         if (!created) {
           result = await user_market.update({ marketId: mart.id }, {
             where: { userId: userSelected.id }
-          }).catch(err=>{res.send(err)})
+          }).catch(err => { res.send(err) })
         }
 
-        res.status(200).send({result,created})
-
-        // res.status(200).send(result);
+        res.status(200).send({ result, created })
       }
-      // oauth회원 주문 처리
+      // 2. 회원분기 처리: oauth회원은 oauth
       else if (req.cookies.userType === "oauth") {
         res.status(200).end()
       }
-    }catch(err){
-        if(err.message ==="jwt must be provided"){
-          //비회원들에게 진행될 코드들
-	try{
-          const unknownId =Number( req.cookies.unknown_id)
+    }
+    // 3. 회원분기 처리: 비회원은 unknown
+    catch (err) {
+      if (err.message === "jwt must be provided") {
+        //비회원들에게 진행될 코드들
+        try {
+          const unknownId = Number(req.cookies.unknown_id)
           const { mobile } = req.body;
 
           let [mart, b] = await market.findOrCreate({
             where: { mobile: mobile },
             defaults: { mobile: mobile }
           })
-	
-	
+
           let [a, created] = await unknown_market.findOrCreate({
-	    attributes:["id","userId","marketId"],
+            attributes: ["id", "userId", "marketId"],
             where: { userId: unknownId },
             defaults: { userId: unknownId, marketId: mart.id }
           })
-       
- 
+
           if (!created) {
             await unknown_market.update({ market_id: mart.id }, {
-            where: { user_id: unknownId }
+              where: { user_id: unknownId }
             });
           }
-          // 마지막으로 보내줄 값 지정필요
-          res.status(200).send({mart,b,unknownId,a})
-         
-        }catch(err){
-         res.status(202).send(err)
+
+          res.status(200).send({ mart, b, unknownId, a })
+
+        } catch (err) {
+          res.status(202).send(err)
         }
       }
     }
--- controllers/totalinfo/index.js
+++ controllers/totalinfo/index.js
@@ -1,35 +1,33 @@
 const jwt = require("jsonwebtoken");
 const { secret } = require('../../config/config')
 const {
-  user, user_order, user_order_item,user_market,
+  user, user_order, user_order_item, user_market,
   // oauth_user, oauth_user_order, oauth_user_order_item,
   unknown, unknown_order, unknown_order_item,
-  item,market
+  item, market
 } = require("../../models");
 module.exports = {
   get: async (req, res) => {
 
     // if(회원){회원들이 이용하는것  if(어스)else if(일반회원)=>cookie.userType}=>AccessToken
     // else {비회원들을 위한 것}
-    try{
+    try {
       //회원들
       const JWT = jwt.verify(req.cookies.accessToken, secret.secret_jwt);
+      // 신원확인
+      const userId = await user.findOne({ where: { userId: JWT.userId } });
 
-        // 신원확인
-      const userId = await user.findOne({ where: { userId: JWT.userId } })
-
-        // 한 유저가 주문한 모든 날짜를 반환
+      // 한 유저가 주문한 모든 날짜를 반환
       const userOrderInfo = await user_order.findAll({
         attributes: ["id", "date"],
         where: { userId: userId.id },
         raw: true
-      })
-      .catch(err => { console.log(err) });
-	    console.log("날짜반환")
+      }).catch(err => { console.log(err) });
+      console.log("날짜반환")
       // 날짜에 해당하는 모든 주문 리스트
       const orderList = await userOrderInfo.reduce(async (acc, obj) => {
         const orderLiEl = await acc;
-	      console.log("주문 리스트")
+        console.log("주문 리스트")
         // option === [ {item_id, quantity}, {item_id, quantity} ]
         const option = await user_order_item.findAll({
           attributes: ["itemId", "quantity"],
@@ -66,40 +64,40 @@ module.exports = {
         }
         return await orderLiEl;
       }, {})
-	    console.log("why")
-	    const market_ = await user_market.findOne({
-	      attributes:["marketId"],
-        where : {userId:userId.id}
-      }).catch(err=>{console.log(err)})
-	    console.log(market_)
-      if(market_===null){
-  	    res.send({orderList:orderList,market:{mobile:""}})
-	    }else{	
+      console.log("why")
+      const market_ = await user_market.findOne({
+        attributes: ["marketId"],
+        where: { userId: userId.id }
+      }).catch(err => { console.log(err) })
+      console.log(market_)
+      if (market_ === null) {
+        res.send({ orderList: orderList, market: { mobile: "" } })
+      } else {
         const market_mobile = await market.findOne({
-	        attributes:["mobile"],
-          where : {id:market_.marketId}
+          attributes: ["mobile"],
+          where: { id: market_.marketId }
         })
-       
-        res.send({orderList:orderList,market:{mobile:market_mobile.mobile}})
-	    }
-        /**
-         * {
-         *  orderList: {
-         *    date1: [{item, quantity, unit}, {item, quantity, unit},{item, quantity, unit},{item, quantity, unit}],
-         *    date2: [{}],
-         * },
-         *  market: string,
-         *  temp: [ {}, {} ]
-         * }
-         */
-    }catch(err){
-      if(err.message ==="jwt must be provided"){
+
+        res.send({ orderList: orderList, market: { mobile: market_mobile.mobile } })
+      }
+      /**
+       * {
+       *  orderList: {
+       *    date1: [{item, quantity, unit}, {item, quantity, unit},{item, quantity, unit},{item, quantity, unit}],
+       *    date2: [{}],
+       * },
+       *  market: string,
+       *  temp: [ {}, {} ]
+       * }
+       */
+    } catch (err) {
+      if (err.message === "jwt must be provided") {
         //비회원들에게 진행될 코드들
         res.status(202).send({
-            orderList:{},
-            market:{
-		mobile:""
-            }
+          orderList: {},
+          market: {
+            mobile: ""
+          }
         })
       }
     }
