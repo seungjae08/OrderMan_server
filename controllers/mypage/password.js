fee10bd4c19f3ffefd726974a5215ad535aadf54
seungjae <seungjae08>
2020-12-14 12:39:32 +0900

    finish

--- controllers/mypage/password.js
+++ controllers/mypage/password.js
@@ -23,10 +23,14 @@ module.exports = {
             .createHmac("sha256", secret.secret_pw)
             .update(newPassword)
             .digest("base64");
+
             if(userData.password === encryptedNow){
+                console.log("why?")
+                console.log(encryptedNow)
                 await user.update({password:encryptedNew},{
                     where:{userId:JWT.userId}
-                }).catch(err=>res.status(202).send(err))
+                }).catch(err=>console.log(err))
+                
                 res.state(200).send({msg : "비밀번호 바꾸기 성공"})
             }else{
                 res.status(202).send({msg:"errNowPassword"})
