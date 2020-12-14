96807942aff5c2bd047d586643e7a25a041d8bc3
seungjae <seungjae08>
2020-12-14 14:01:56 +0900

    modify mobile

--- controllers/mypage/mobile.js
+++ controllers/mypage/mobile.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const {secret} = require("../../config/config");
 const { user } = require("../../models");
 
 module.exports={
@@ -6,10 +7,12 @@ module.exports={
         try{
             const {mobile} = req.body 
             const JWT = jwt.verify(req.cookies.accessToken,secret.secret_jwt);
+            
             await user.update({mobile:mobile},{
                 where:{userId:JWT.userId}
-            })
-            res.status(200).send({state:true})
+            }).catch(err=>console.log(err))
+            
+            res.status(200).json({state:true})
         }catch(err){
             res.status(404).send(err)
         }
